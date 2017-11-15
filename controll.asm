a3804142afb55eba4789e86df9b54fb573081d76
fangfcg <thssss15_fangcg@163.com>
2017-11-16 00:07:09 +0800

    for pull and create controll as well as handlers

--- controll.asm
+++ controll.asm
@@ -3,6 +3,7 @@
 option casemap :none                    ; case sensitive
 
 include controll.inc
+include key_handlers.inc
 
 includelib masm32.lib
 includelib kernel32.lib
@@ -11,30 +12,116 @@ includelib msvcrt.lib
 endl EQU <0dh, 0ah>
 
 .data
-outstr1 db "head:%x", endl, 0
-outstr2 db "data:%s", endl, 0
-outstrtest db "count : %d", endl, 0
-program_status DWORD 0
+program_status DWORD 0   ;0: watching  1:editing 2:commanding
+record_cursor_max_index DWORD 0
+text_list:LinkedList
 
+;CURSOR_POSITION_LOGIC STRUCT
+;	p_node DWORD ?
+;	index_char DWORD ?
+;CURSOR_POSITION_LOGIC ENDS
+;EXTERNDEF text_list:List
+;EXTERNDEF window_size:COORD
+;EXTERNDEF cursor_position_ui:COORD
+;EXTERNDEF cursor_position_logic:CURSOR_POSITION_LOGIC
 
-keyInputHandle PROC, keyInput: KEY_INPUT
+;KEY_INPUT STRUCT
+;  is_special BOOL ?
+;  virtual_key WORD ?	;see wVirtualKeyCode field in KEY_EVENT_RECORD
+;  ascii_char BYTE ?
+;KEY_INPUT ENDS
+
+keyInputHandle PROC
     pushad
-        .if program_status == 0
-            invoke 
+    invoke GetUserKeyInput
+    .if eax == 0
+        jmp quit
+    .endif
+
+    .if program_status == 0
+        ;watching
+    .elseif program_status == 1
+        ;editing
+        invoke EditingStatusHandle
+    .else
+        ;commanding
+    .endif
+quit:
     popad
     ret
 keyInputHandle ENDP
 
-EditingStatusHandle PROC, keyInput: KEY_INPUT
+
+EditingStatusHandle PROC
+    local key_input: KEY_INPUT
+    pushad
+    invoke PopInputQueue, addr key_input
+
+    .if key_input.is_special == 0
+        ;normal char
+        invoke NormalKeyDeal, key_input    
+    .else
+        ;special key
+        invoke SpecialKeyDeal, key_input
+    .endif
+
+
+
+
+
+    ;need redraw here
+quit:
+    popad
+    ret
+
+
 
 EditingStatusHandle ENDP
 
 
 
-WatchingStatusHandle PROC, keyInput: KEY_INPUT
+NormalKeyDeal PROC, key_input: KEY_INPUT
+    pushad
 
-WatchingStatusHandle ENDP
+        mov edi, cursor_position_logic.p_node
+        mov eax, cursor_position_logic.index_char
+        mov esi, (Node PTR [edi]).data
+        invoke InsertChar esi, key_input.ascii_char, cursor_position_logic.index_char
+        inc cursor_position_logic.index_char
+        mov ax, window_size.x
+        mov bx, window_size.y
+        .if ax == cursor_position_ui.x
+            mov cursor_position_ui.x, 0
+            .if bx > cursor_position_ui.y
+                inc cursor_position_ui.y
+            .endif
+        .else
+            inc cursor_position_ui.x
+        .endif
 
-CommandStatusHandle PROC, keyInput: KEY_INPUT
+    popad
+    ret
+NormalKeyDeal ENDP
 
-CommandStatusHandle ENDP
+
+SpecialKeyDeal PROC, key_input: KEY_INPUT
+    pushad
+        mov ax, key_input.virtual_key
+        .if ax == VK_LEFT
+            invoke LeftKeyHandler
+
+
+        .elseif ax == VK_RIGHT
+            invoke RightKeyHandler
+
+        .elseif ax == VK_UP
+            invoke UpKeyHandler
+
+        .elseif ax == VK_DOWN
+            invoke DownKeyHandler
+        .endif
+
+quit:
+    popad
+    ret
+SpecialKeyDeal ENDP
---
+++ directionkey_handlers.asm
@@ -0,0 +1,132 @@
+.486                                    ; create 32 bit code
+.model flat, stdcall                    ; 32 bit memory model
+option casemap :none                    ; case sensitive
+
+include linkedList.inc
+include input_queue.inc
+include ui.inc
+include controll.inc
+
+UpKeyHandler PROC 
+    pushad
+        mov edi, cursor_position_logic.p_node
+        mov esi, (Node PTR [edi]).prev
+        mov ebx, (Node PTR [esi]).prev
+        .if ebx == NULL
+        ; previous node is head
+            jmp quit
+        .endif
+        mov cursor_position_logic.p_node, esi
+        mov ecx, cursor_position_logic.index_char
+        .if ecx < record_cursor_max_index
+            mov ecx, record_cursor_max_index
+        .else
+            mov record_cursor_max_index, ecx
+        .endif
+        .if (Node PTR [esi]).datalength >= ecx
+            mov cursor_position_logic.index_char, ecx
+        .else
+            mov ecx, (Node PTR [esi]).datalength
+            mov cursor_position_logic.index_char, ecx
+        .endif
+    popad
+    ret
+UpKeyHandler ENDP
+
+LeftKeyHandler PROC 
+    pushad
+
+        mov record_cursor_max_index, 0
+        .if cursor_position_logic.index_char > 0
+        ;if cursor isn't at the beginning of logic line
+            dec cursor_position_logic.index_char 
+            .if cursor_position_ui.x > 0
+                dec cursor_position_ui.x
+            .else
+                mov bx, window_size.x
+                dec bx
+                mov cursor_position_ui.x, bx
+                .if cursor_position_ui.y > 0
+                    dec cursor_position_ui.y
+                .endif
+            .endif
+        .else
+        ;if cursor is at the beginning of logic line            
+            mov edi, cursor_position_logic.p_node
+            mov esi, (Node PTR [edi]).prev
+            mov ebx, (Node PTR [esi]).prev
+            .if ebx == NULL
+            ;if prevnode is head
+                jmp quit
+            .endif
+            mov cursor_position_logic.p_node, esi
+            mov ebx, (Node PTR [esi]).data.datalength
+            dec ebx ;ebx = datalength - 1
+            mov cursor_position_logic.index_char, ebx ;index_char = prevnode->length - 1
+            
+            movzx ecx, window_size.x
+            pushad
+                mov edx, 0
+                mov eax, ebx                         
+                div ecx                             ;cursor_position_ui.x = (datalength - 1) % window_size.x
+                mov cursor_position_ui.x, dx    
+            popad
+            .if cursor_position_ui.y > 0
+                dec cursor_position_ui.y
+            .endif
+
+        .endif
+
+    popad
+    ret
+LeftKeyHandler ENDP
+
+RightKeyHandler PROC 
+    pushad
+        mov record_cursor_max_index, 0
+        mov edi, cursor_position_logic.p_node
+        mov ebx, (Node PTR [edi]).data.datalength
+        dec ebx ;ebx = datalength - 1
+
+        .if cursor_position_logic.index_char < ebx
+        ;if cursor isn't at the end of logic line
+            inc cursor_position_logic.index_char
+            mov cx, window_size.x
+            dec cx 
+            .if cursor_position_ui.x < cx
+                inc cursor_position_ui.x
+            .else
+                mov cursor_position_ui.x, 0
+                mov dx, window_size.y
+                dec dx
+                .if cursor_position_ui.y < dx
+                    inc cursor_position_ui.y
+                .endif
+            .endif
+        
+        .else
+            ;if cursor is at the end of logic line
+            mov esi, (Node PTR[esi]).next
+            .if esi == NULL
+                jmp quit
+            .endif
+            mov cursor_position_logic.p_node, esi
+            mov cursor_position_logic.index_char, 0
+            mov cursor_position_ui.x, 0
+            mov dx, window_size.y
+            dec dx
+            .if cursor_position_ui.y < dx
+                inc cursor_position_ui.y
+            .endif
+
+        .endif
+    popad
+    ret
+RightKeyHandler ENDP
+
+DownKeyHandler PROC 
+    pushad
+        
+    popad
+    ret
+DownKeyHandler ENDP
